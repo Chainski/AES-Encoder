d3f805da21c7466cb420bf1dcef584a177d9a4dd
Chainski <96607632+Chainski@users.noreply.github.com>
2022-10-22 11:56:24 -0500

    Add files via upload

--- AES-Encoder.ps1
+++ AES-Encoder.ps1
@@ -28,8 +28,8 @@ function Create-Var() {
 
 function RAND() {
 
-        $set = "xQpVQLuQpVQpVQpVQpVQWpVQpVQpVQpVQpVQpVQpKVQpVQpVQpVQpV"
-        (1..(6 + (Get-Random -minimum 8 -Maximum 10)) | %{ $set[(Get-Random -Minimum 10 -Maximum $set.Length)] } ) -join ''
+        $set = "xQpVQLuQpVQpVQpTVLQpVQyWpZPVQpkHVQpVQpVJQpGVQpVQpKVXQpVQpVQpVQpV"
+        (1..(7 + (Get-Random -minimum 9 -Maximum 12)) | %{ $set[(Get-Random -Minimum 10 -Maximum $set.Length)] } ) -join ''
 }
 
 function Invoke-AES-Encoder {
@@ -125,16 +125,16 @@ sleep 1
             Write-Host "[*] Generating Encryption Key ..." 
             
 			$aesManaged = New-Object "System.Security.Cryptography.AesManaged"
-            $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
+            $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CbC
             
-			if ($paddingmode -eq 'PKCS7') {
-                $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::PKCS7
-            } elseif ($paddingmode -eq 'ISO10126') {
-                $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::ISO10126
-            } elseif ($paddingmode -eq 'ANSIX923') {
-                $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::ANSIX923
-            } elseif ($paddingmode -eq 'Zeros') {
-                $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+			if ($paddingmode -eq 'PkCs7') {
+                $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::PkCs7
+            } elseif ($paddingmode -eq 'IsO10126') {
+                $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::IsO10126
+            } elseif ($paddingmode -eq 'AnSIx923') {
+                $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::AnSIx923
+            } elseif ($paddingmode -eq 'ZeRoS') {
+                $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::ZeRoS
             }
             
 			$aesManaged.BlockSize = 128
@@ -159,7 +159,7 @@ sleep 1
             $stub_template = ''
 
             $code_alternatives  = @()
-			 $code_alternatives += '([SYstEm.teXt.encODIng]::uTf8.gETSTriNG([sysTEM.cONVErT]::fRomBAse64StRInG("QWRkLVR5cGUgLUFzc2VtYmx5TmFtZSBTeXN0ZW0uV2ViID4gJG51bGw="))) | iex' + "`r`n"
+			 $code_alternatives += '([REGex]::uNesCapE("\u0041\u0064\u0064\u002d\u0054\u0079\u0070\u0065\u0020\u002d\u0041\u0073\u0073\u0065\u006d\u0062\u006c\u0079\u004e\u0061\u006d\u0065\u0020\u0053\u0079\u0073\u0074\u0065\u006d\u002e\u0057\u0065\u0062\u0020\u003e\u0020\u0024\u006e\u0075\u006c\u006c")) | iEx' + "`r`n"
             $code_alternatives += '${2} = [coNVeRT]::fROmbASE64sTRINg("{0}")' + "`r`n"       			
 		   $code_alternatives += '${2} = [coNVeRT]::fROmbASE64sTRINg("{0}")' + "`r`n"
             $code_alternatives += '${3} = [coNVeRT]::fRomBaSE64sTRINg("{1}")' + "`r`n"
@@ -168,24 +168,24 @@ sleep 1
             $stub_template += $code_alternatives_shuffled -join ''
            
 		   $code_alternatives  = @()
-            $code_alternatives += '${4}.ModE = [REGex]::uNesCapE("\u005b\u0053\u0079\u0073\u0074\u0065\u006d\u002e\u0053\u0065\u0063\u0075\u0072\u0069\u0074\u0079\u002e\u0043\u0072\u0079\u0070\u0074\u006f\u0067\u0072\u0061\u0070\u0068\u0079\u002e\u0043\u0069\u0070\u0068\u0065\u0072\u004d\u006f\u0064\u0065\u005d\u003a\u003a\u0043\u0042\u0043") | iex' + "`r`n"
+            $code_alternatives += '${4}.ModE = [SYSTem.SecUriTy.CrYPTOGrapHY.cIpheRmodE]::'+$CIpHerMoDE + "`r`n"
             $code_alternatives += '${4}.Padding = [sYsTem.SECuRIty.cRYptOGRaPhy.PaDdiNgMoDe]::'+$paddingmode + "`r`n"
-            $code_alternatives += '${4}.BlOckSIze = [SySTEm.NET.WeButiliTy]::hTmLdEcOdE("&#x31;&#x32;&#x38;") | iex' + "`r`n"         			
+            $code_alternatives += '${4}.BlOckSIze = [SySTEm.NEt.WeButiliTy]::hTmLdEcOdE("&#x31;&#x32;&#x38;") | Iex' + "`r`n"         			
             $code_alternatives += '${4}.KeySize = '+$keysize + "`n" + '${4}.Key = ${3}' + "`r`n"
             $code_alternatives += '${4}.Iv = ${2}[0..15]' + "`r`n"
             $code_alternatives_shuffled = $code_alternatives 
             $stub_template += $code_alternatives_shuffled -join ''
 
             $code_alternatives  = @()
-            $code_alternatives += '${6} = nEw-OBJECt sySTeM.io.mEmorYSTrEam(,${4}.CrEAtedECrYptor().TRaNsFOrmfinaLBlOCk(${2},16,${2}.LEnGth-16))' + "`r`n"
-            $code_alternatives += '${7} = [RegEX]::UnESCaPe("\u004e\u0065\u0077\u002d\u004f\u0062\u006a\u0065\u0063\u0074\u0020\u0053\u0079\u0073\u0074\u0065\u006d\u002e\u0049\u004f\u002e\u004d\u0065\u006d\u006f\u0072\u0079\u0053\u0074\u0072\u0065\u0061\u006d") | iex' + "`r`n"
+            $code_alternatives += '${6} = nEw-OBJECt ([REGex]::uNesCapE("\u0073\u0079\u0053\u0054\u0065\u004d\u002e\u0069\u006f\u002e\u006d\u0045\u006d\u006f\u0072\u0059\u0053\u0054\u0072\u0045\u0061\u006d"))(,${4}.CrEAtedECrYptor().TRaNsFOrmfinaLBlOCk(${2},16,${2}.LEnGth-16))' + "`r`n"
+            $code_alternatives += '${7} = [RegEX]::UnESCaPe("\u004e\u0065\u0077\u002d\u004f\u0062\u006a\u0065\u0063\u0074\u0020\u0053\u0079\u0073\u0074\u0065\u006d\u002e\u0049\u004f\u002e\u004d\u0065\u006d\u006f\u0072\u0079\u0053\u0074\u0072\u0065\u0061\u006d") | ieX' + "`r`n"
 			$code_alternatives_shuffled = $code_alternatives 
             $stub_template += $code_alternatives_shuffled -join ''
 
             if ($compressiontype -eq "Gzip") {
-                $stub_template += '${5} = nEw-oBject sYSTEM.IO.cOmPreSsiOn.gZIpStReAM ${6}, ([IO.Compression.CompressionMode]::Decompress)' + "`r`n"
+                $stub_template += '${5} = nEw-oBject ([SYSTeM.web.httpUTiLIty]::urLdeCOdE("sYSTEM%2EIO%2EcOmPreSsiOn%2EgZIpStReAM%20%20")) ${6}, ([io.CompREsSION.COmPrEsSionMOdE]::DecompReSs)' + "`r`n"
             } elseif ( $compressiontype -eq "Deflate") {
-                $stub_template += '${5} = NEw-oBject SySteM.iO.ComprESsIoN.DEfLAtEsTReAM ${6}, ([io.CompREsSION.COmPressionMOdE]::DecompReSs)' + "`r`n"
+                $stub_template += '${5} = NEw-oBject ([SYSTeM.web.httpUTiLIty]::urLdEcOdE("SySteM%2EiO%2EComprESsIoN%2EDEfLAtEsTReAM")) ${6}, ([io.CompREsSION.COmPrEssionMOdE]::DecompReSs)' + "`r`n"
             }
             $stub_template += '${5}.CoPyTo(${7})' + "`r`n"
 
@@ -196,11 +196,11 @@ sleep 1
             $code_alternatives += '${8} = [sYStem.texT.enCoDIng]::uTF8.GETstrInG(${7}.tOArraY())' + "`r`n"
             $code_alternatives_shuffled = $code_alternatives 
             $stub_template += $code_alternatives_shuffled -join ''
-            $stub_template += ('INVoke-ExPREsSion','IeX' | Get-Random)+'(${8})' + "`r`n"
+            $stub_template += ('INVoke-ExPREsSion','IeX','iEx','InVokE-ExPREsSion' | Get-Random)+'(${8})' + "`r`n"
             
         
             # Concatenating each value manually.
-            $code = $stub_template -f $b64encrypted, $b64key, (Create-Var), (Create-Var), (RAND), (Create-Var), (Create-Var), (Create-Var), (Create-Var), (Create-Var)
+            $code = $stub_template -f $b64encrypted, $b64key, (Create-Var), (RAND), (Create-Var), (RAND), (Create-Var), (RAND), (Create-Var), (RAND)
             $codebytes = [System.Text.Encoding]::UTF8.GetBytes($code)
         }
         Write-Host "[*] Writing '$($outfile)' ..." 
